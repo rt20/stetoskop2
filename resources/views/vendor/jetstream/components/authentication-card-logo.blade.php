4802a16e76644e0123596b722d06f49b6875e0fb
rt20 <kaploks@gmail.com>
2022-03-31 20:54:02 +0700

    first commit kedua

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-    <img src="https://miro.medium.com/max/1200/1*anSazwoTMKra5IuiJumjng.png" alt="" style="width: 100px; height:100px;">
+    <img src="{{ asset('img/logo.jpeg') }}" alt="" style="width: 100px; height:100px;">
 </a>
