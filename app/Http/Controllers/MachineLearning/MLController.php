da07f297cc263e87cfb732cfb5ce991d9ab45932
rt20 <kaploks@gmail.com>
2022-04-03 16:35:39 +0700

    change path ke-10

--- app/Http/Controllers/MachineLearning/MLController.php
+++ app/Http/Controllers/MachineLearning/MLController.php
@@ -24,7 +24,8 @@ class MLController extends Controller
         if (Storage::disk('public')->exists("uploads/$request->name")) {
             $data = Storage::disk('public')->path("uploads/$request->name");
             $model = '/model.tflite';
-            $python = 'python';
+            dd($model);
+            $python = '/usr/bin/python3.8';
             $script = escapeshellcmd(base_path('/ml_scrypt.py'));
             $command = "$python $script $data $model";
             ob_start();
@@ -89,7 +90,7 @@ class MLController extends Controller
         if (Storage::disk('public')->exists("uploads/$readpasien->name")) {
             $data = Storage::disk('public')->path("uploads/$readpasien->name");
             $model = '/model.tflite';
-            $python = 'python';
+            $python = '/usr/bin/python3.8';
             $script = escapeshellcmd(base_path('/ml_scrypt.py'));
             $command = "$python $script $data $model";
             ob_start();
